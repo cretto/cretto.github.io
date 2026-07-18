0676932f838ae344109336243d6245603e21c34c
Cretto <henrique.anacretto.pereira@gmail.com>
2026-07-18 16:13:08 -0300

    create gh workflow

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,7 +6,7 @@ import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
   title: "Henrique Anacretto Pereira",
-  tagline: "Dinosaurs are cool",
+  tagline: "Engineering Portfolio",
   favicon: "img/favicon.ico",
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -15,15 +15,17 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://cretto.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "cretto", // Usually your GitHub org/user name.
+  projectName: "cretto.github.io", // Usually your repo name.
+
+  deploymentBranch: "gh-pages",
 
   onBrokenLinks: "throw",
 
